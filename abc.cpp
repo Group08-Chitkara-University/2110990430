8eb609a479c8a33a372af0eb8f9823743a7bc0d4
devwani0430 <devwani0430.be21@chitkara.edu.in>
2022-04-15 21:49:10 +0530

    Add conditions to the code

--- abc.cpp
+++ abc.cpp
@@ -1,5 +1,15 @@
 #include<iostream>
 using namespace std;
 int main(){
-	cout<<abc;
+	int n;
+	cout<<"Enter a num = ";
+	cin>>n;
+	if(n%2==0)
+	{
+		cout<<n<<" is a even number";
+	}
+	else{
+		cout<<n<<" is a odd numer";
+	}
+	return 0;
 }
